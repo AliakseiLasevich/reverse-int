a3c57972ddf3e1c01e6bb2a2d0a381e2486419bf
AliakseiLasevich <ababaka@gmail.com>
2020-02-21 18:42:07 +0300

    feat: reverse function released

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    if (n < 0) {
+        n *= -1;
+    }
+    let array = n.toString().split("");
+    let reverse = [];
+    for (let i = array.length - 1; i > -1; i--) {
+        reverse.push(+array[i]);
+    }
+    return reverse.join("");
 }
